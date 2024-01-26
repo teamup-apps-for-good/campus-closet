bcc8ba0168b74aa046e36112a10a2224afed9821
hunterpearson36 <hunterpearson36@gmail.com>
2024-01-26 16:46:55 -0600

    edit cucumber tests to avoid rubocop issue

--- features/step_definitions/upload_clothing.rb
+++ features/step_definitions/upload_clothing.rb
@@ -12,17 +12,6 @@ When('I am on the new item page') do
   expect(page).to have_current_path('/items/new')
 end
 
-And('I fill in the fields with {string}, {string}, {string}, {string}, {string}, {string}, {string}') do
-    |color, type, desc, gender, size, condition, status| # rubocop:disable Layout/ParameterLists
-  select(color, from: 'item_color_id')
-  select(type, from: 'item_type_id')
-  fill_in 'item_description', with: desc
-  select(gender, from: 'item_gender_id')
-  select(size, from: 'item_size_id')
-  select(condition, from: 'item_condition_id')
-  select(status, from: 'item_status_id')
-end
-
 Then('there should be a new item in the items page, {int}') do |int|
   items = Item.count + 1
   expect(items).to eq(int)
@@ -32,8 +21,8 @@ And('I include a picture') do
   attach_file('item_image', Rails.root.join('features', 'test_image', 'shirt.jpg'))
 end
 
-When('I fill in type with {string}') do |type|
-  select(type, from: 'item_type_id')
+When('I fill in {string} with {string}') do |cat, value|
+  select(value, from: "item_#{cat}_id")
 end
 
 Then('type should still be {string}') do |type|
@@ -42,4 +31,8 @@ end
 
 But('there should not be a picture') do
   expect(page).to have_content('No image available')
+end
+
+When('I fill in description with {string}') do |desc|
+  fill_in 'item_description', with: desc
 end
