ed4cb795ddcbf99407c1f4d94f544c9a05b75f60
jds7576 <jds7576@tamu.edu>
2024-02-21 18:00:46 -0600

    ruby controllers

--- app/controllers/chat_page_controller.rb
+++ app/controllers/chat_page_controller.rb
@@ -1,4 +1,8 @@
 class ChatPageController < ApplicationController
+  # before_action :authenticate_user!
+  
   def home
+    @messages = Message.last(5)
+    @message = current_user.messages.build
   end
 end
--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,2 +1,23 @@
 class MessagesController < ApplicationController
+    # before_action :authenticate_user!
+    before_action :set_message, only: [:destroy]
+
+    def create # creates message
+        @message = current_user.messages.build(message_params)
+        @message.save
+    end
+
+    def destroy
+        @message.destroy
+    end
+
+    private
+
+    def message_params # require message data
+        params.require(:message).permit(:body)
+    end
+
+    def set_message # find message by id
+        @message = Message.find(params[:id])
+    end
 end
