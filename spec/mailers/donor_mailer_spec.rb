4d64e9fc087da83d9e2f011ccadf4e8b12968413
James Nolan <jamesn8391@gmail.com>
2024-02-23 11:59:02 -0600

    add donor_mailer

---
+++ spec/mailers/donor_mailer_spec.rb
@@ -0,0 +1,19 @@
+# spec/mailers/donor_mailer_spec.rb
+require 'rails_helper'
+
+RSpec.describe DonorMailer, type: :mailer do
+  describe 'confirm_time_email' do
+    let(:user) { User.create(first: 'Example User') }
+    let(:mail) { described_class.confirm_time_email(user: user).deliver_now }
+
+    it 'renders the headers' do
+      expect(mail.subject).to eq('Welcome to My Awesome Site')
+      expect(mail.to).to eq([user.email])
+      expect(mail.from).to eq(['notifications@example.com'])
+    end
+
+    it 'renders the body' do
+      expect(mail.body.encoded).to match('Welcome to My Awesome Site')
+    end
+  end
+end
