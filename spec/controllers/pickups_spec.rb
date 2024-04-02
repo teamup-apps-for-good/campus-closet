635cb7baa51f50ba2895be2642649e2d3b8d0675
hunterpearson36 <hunterpearson36@gmail.com>
2024-04-02 04:24:05 -0500

    fixing rspec tests

--- spec/controllers/pickups_spec.rb
+++ spec/controllers/pickups_spec.rb
@@ -134,7 +134,3 @@ end
 RSpec.describe PickupsController, type: :controller do
   it_behaves_like 'another CRUD controller', Pickup, PickupsController
 end
-
-RSpec.describe RequestsController, type: :controller do
-  it_behaves_like 'another CRUD controller', Request, RequestsController
-end
